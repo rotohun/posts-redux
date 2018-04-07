9e58bd21e55915fe55e767bedc26d1c7885d280b
Ronald Hunter <ronaldtoddhunter@yahoo.com>
2018-04-06 21:24:55 -0400

    Added POST GET methods to post form

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
 
 class Posts extends Component {
+
+  constructor(props){
+    super(props)
+    this.state ={
+      posts: [
+
+      ]
+    }
+  }
   
   componentWillMount(){
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then( res => res.json())
-    .then( data => console.log(data))
+    .then( data => this.setState({posts: data}))
   }
 
   render() {
+    const postItems = this.state.posts.map( posts => (
+      <div key={posts.id}>
+        <h3>{posts.title}</h3>
+        <p>{posts.body}</p>
+      </div>
+    ))
     return (
       <div>
-        <h1> Posts </h1>
+        { postItems }
       </div>
     )
   }
